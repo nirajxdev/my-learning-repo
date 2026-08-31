f2f153ad03c17baf995258e39f0dcad14f7dedc0
nirajxdev <nirajcs2023@gmail.com>
2026-08-31 14:24:48 +0530

    add some more lines

--- react/01-project/src/App.jsx
+++ react/01-project/src/App.jsx
@@ -1,8 +1,8 @@
-import About from "./components/About";
-import Footer from "./components/Footer";
-import Hero from "./components/Hero";
-import Navbar from "./components/Navbar";
-import Skills from "./components/Skills";
+// import About from "./components/About";
+// import Footer from "./components/Footer";
+// import Hero from "./components/Hero";
+// import Navbar from "./components/Navbar";
+// import Skills from "./components/Skills";
 import ProfileCard from "./components/ProfileCard";
 import ProductCard from "./components/ProuductCard";
 import Counter from "./components/Counter";
