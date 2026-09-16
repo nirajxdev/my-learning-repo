b9c214088c366b63becdb4f3d4843fbae0afc966
nirajxdev <nirajcs2023@gmail.com>
2026-09-16 18:56:26 +0530

    initial typescript things

---
+++ typescript/index.ts
@@ -0,0 +1,11 @@
+function greet(first_name: string) {
+  console.log("hello " + first_name);
+}
+
+greet("Mayank");
+
+
+function sum(a: number, b: number) {
+  return a + b;
+}
+console.log(sum(1, 3));
