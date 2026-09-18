6155528d00e99420c98cb6dff2a0f5a09c83d773
nirajxdev <nirajcs2023@gmail.com>
2026-09-18 06:11:54 +0530

    added lesson1

---
+++ typescript/lesson1.ts
@@ -0,0 +1,22 @@
+let username = "Niraj";
+let age = 19;
+let isStudent = false;
+
+// 1. what will happen for username = 123;
+// username = 123;
+
+// 2. what happens for age = "nineteen";
+// age = "nineteen";
+
+// 3. what happens for isStudent = "no";
+// isStudent = "no";
+
+// 4. create an array that can contain only numbers:
+let ids: number[] = [1, 2, 3, 4, 5];
+console.log(ids);
+
+// 5. create a variable called *id* that can contain either a number or string
+let id: string | number;
+id = "abc";
+id = 124
+console.log(id);
