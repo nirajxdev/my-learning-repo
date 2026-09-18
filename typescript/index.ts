f91b4c4d4cfc5ecf8beea6e934d6285b39051532
nirajxdev <nirajcs2023@gmail.com>
2026-09-18 23:54:25 +0530

    update

--- typescript/index.ts
+++ typescript/index.ts
@@ -1,11 +1,39 @@
-function greet(first_name: string) {
-  console.log("hello " + first_name);
+// function add(a: number, b: number): number {
+//   return a + b;
+// }
+
+// console.log(add(1, 2));
+//
+
+// function greet(name: string) {
+//   console.log("hello " +name);
+// }
+
+// greet("niraj");
+//
+// type Task = {
+//   id: number;
+//   name: string;
+//   done: boolean;
+// }
+
+// function createTask(name: string): Task {
+//   return {
+//     id: 1,
+//     name: name,
+//     done: false,
+//   };
+// }
+
+// const task = createTask("learn typescript");
+// console.log(task);
+//
+//
+//
+
+//interface
+interface Task {
+  id: number;
+  name: string;
+  done: boolean;
 }
-
-greet("Mayank");
-
-
-function sum(a: number, b: number) {
-  return a + b;
-}
-console.log(sum(1, 3));
